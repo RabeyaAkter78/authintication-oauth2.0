def016d4f433892a77b2cd8c17110b1097a8779f
Rabeya Akter <rabeyariya251@gmail.com>
2025-02-08 11:01:00 +0600

    problem on authinticate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-
+// import { useAuth0 } from "@auth0/auth0-react";
 export default function Home() {
-
+  // const { loginWithRedirect } = useAuth0();
   return (
     <main>
       <div className='flex justify-center my-52'>
-        <button className='px-4 py-2 rounded-xl bg-green-500 text-white text-center'>Login with Auth2.0</button>
+        <button  className='px-4 py-2 rounded-xl bg-green-500 text-white text-center'>Login with Auth2.0</button>
       </div>
     </main>
   );
